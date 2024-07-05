17b0d1681be94a881a82248a92197a3e07c11ead
ChikaDibia <ogbuchika71@gmail.com>
2024-07-05 16:53:59 +0100

    Fix bugs

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,15 +1,15 @@
 
 const Users = ({ data }) => {
   return (
-    <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-4">
+    <div className="flex flex-col items-center w-full max-w-md bg-white rounded-lg shadow-lg p-4">
       <h2 className="text-2xl font-bold mb-4">User List</h2>
-      <ul className="divide-y divide-gray-200">
+      <ul className="flex flex-wrap divide-y divide-gray-200">
         {data.map((user) => (
           <li key={user.id} className="py-4">
-            <div className="flex items-center justify-between">
-              <div>
+            <div className="flex flex-col items-center justify-center bg-[#9d9dfa] w-[320px] h-[230px] rounded-lg">
+              <div className="flex flex-col items-center justify-center text-white">
                 <h3 className="text-lg font-medium">{user.name}</h3>
-                <p className="text-sm text-gray-500">{user.email}</p>
+                <p className="text-md text-gray-500">{user.email}</p>
               </div>
             </div>
           </li>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -51,12 +51,12 @@ const Login = () => {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm"
+        className="bg-[#797a77] p-6 rounded-lg shadow-lg w-full max-w-sm"
       >
         <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-black text-sm font-bold mb-2"
             htmlFor="email"
           >
             Email
@@ -74,7 +74,7 @@ const Login = () => {
         </div>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-black text-sm font-bold mb-2"
             htmlFor="password"
           >
             Password
@@ -92,7 +92,7 @@ const Login = () => {
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full bg-[#0f620f] text-white py-2 px-4 rounded-md hover:bg-[green] focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Submit
         </button>
