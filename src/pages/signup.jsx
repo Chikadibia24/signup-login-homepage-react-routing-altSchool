d74b46a7f8a9f80bafe40fc52cfdd7a295d4f752
ChikaDibia <ogbuchika71@gmail.com>
2024-07-05 16:20:48 +0100

    Second commit

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -46,12 +46,12 @@ const Signup = () => {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm"
+        className="bg-[#797a77] p-6 rounded-lg shadow-lg w-full max-w-sm"
       >
         <h2 className="text-2xl font-bold mb-4 text-center">Sign Up</h2>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-black text-sm font-bold mb-2"
             htmlFor="name"
           >
             Name
@@ -69,7 +69,7 @@ const Signup = () => {
         </div>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-black text-sm font-bold mb-2"
             htmlFor="email"
           >
             Email
@@ -87,7 +87,7 @@ const Signup = () => {
         </div>
         <div className="mb-4">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-black text-sm font-bold mb-2"
             htmlFor="password"
           >
             Password
@@ -105,7 +105,7 @@ const Signup = () => {
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full bg-[#0f620f] text-white py-2 px-4 rounded-md hover:bg-[green] focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Submit
         </button>
