6ef6159b8a7b43408835203bf93bb6491b2834d1
ChikaDibia <ogbuchika71@gmail.com>
2024-07-05 17:20:44 +0100

    Design homepage layout

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,12 +1,12 @@
 
 const Users = ({ data }) => {
   return (
-    <div className="flex flex-col items-center w-full max-w-md bg-white rounded-lg shadow-lg p-4">
+    <div className="flex flex-col items-center w-full justify-center bg-[rgb(234,249,198)] rounded-lg shadow-lg p-4">
       <h2 className="text-2xl font-bold mb-4">User List</h2>
-      <ul className="flex flex-wrap divide-y divide-gray-200">
+      <ul className="flex gap-3 justify-center flex-wrap divide-y divide-gray-200">
         {data.map((user) => (
           <li key={user.id} className="py-4">
-            <div className="flex flex-col items-center justify-center bg-[#9d9dfa] w-[320px] h-[230px] rounded-lg">
+            <div className="flex flex-col items-center justify-center bg-[#09052f] w-[320px] h-[230px] rounded-lg">
               <div className="flex flex-col items-center justify-center text-white">
                 <h3 className="text-lg font-medium">{user.name}</h3>
                 <p className="text-md text-gray-500">{user.email}</p>
