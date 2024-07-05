c7191c17717d93f79fc31347cf6c2257d6ea045b
ChikaDibia <ogbuchika71@gmail.com>
2024-07-05 13:45:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import HomePage from "./pages/homepage";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
